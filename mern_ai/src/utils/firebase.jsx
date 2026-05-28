7fd1c56bc1c87a6767acbe225f0bb8013baa3277
vish3456 <vishnu.singh_cs.h24@gla.ac.in>
2026-05-28 22:46:56 +0530

    Firebase SDK and configure authentication services

--- mern_ai/src/utils/firebase.jsx
+++ mern_ai/src/utils/firebase.jsx
@@ -1,20 +1,26 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAazTU3sUcUMuyLVqLOAWczOdXB8HIetq4",
+  apiKey: "AIzaSyDq5g9maNY8zjJDzhF351TTT0l9GQCC2PU",
   authDomain: "resume-54303.firebaseapp.com",
   projectId: "resume-54303",
   storageBucket: "resume-54303.firebasestorage.app",
-  messagingSenderId: "235206334441",
-  appId: "1:235206334441:web:7116ca0325274d99c01d78",
-  measurementId: "G-W98V3XNVJX"
+  messagingSenderId: "1001646383698",
+  appId: "1:1001646383698:web:a239e8a35443820238b58b",
+  measurementId: "G-S0SSNRFE4M"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
 
 export { auth, provider };
