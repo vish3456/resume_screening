8a0e1f00375fa45141f96b7ce1cd87ad052ec60c
vish3456 <vishnu.singh_cs.h24@gla.ac.in>
2026-05-28 22:26:52 +0530

    Firebase app and export authentication services

--- mern_ai/src/utils/firebase.jsx
+++ mern_ai/src/utils/firebase.jsx
@@ -4,9 +4,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAazTU3sUcUMuyLVqLOAWczOdXB8HIetq4",
-  authDomain: "mernai-b1525.firebaseapp.com",
-  projectId: "mernai-b1525",
-  storageBucket: "mernai-b1525.firebasestorage.app",
+  authDomain: "resume-54303.firebaseapp.com",
+  projectId: "resume-54303",
+  storageBucket: "resume-54303.firebasestorage.app",
   messagingSenderId: "235206334441",
   appId: "1:235206334441:web:7116ca0325274d99c01d78",
   measurementId: "G-W98V3XNVJX"
